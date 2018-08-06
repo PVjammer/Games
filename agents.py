99dc2db92685985196010403b131498761cc841d
Nicholas Burnett <nicholasburnett@datamachines.io>
2018-08-06 16:11:09 -0400

    Update

--- agents.py
+++ agents.py
@@ -1,4 +1,6 @@
 import numpy as np
+from Connect4.rules import Connect4Rules
+from HeuristicAgent.simple_functions import *
 
 
 def load_config(file_name):
@@ -31,3 +33,45 @@ class HumanAgent:
             # print (e)
             print("You're an idiot, I'm choosing for you!")
             return np.random.randint(self.n_actions)
+
+class HeuristicAgent:
+    """
+    General Agent which implements minimax with A/B pruning and evalates a user
+    defined objective function (or a library function) at maximum depth.
+    """
+    def __init__(self, eval_func=None, config=None, game_rules=Connect4Rules()):
+        if obj_func is not none:
+            self.eval_function = obj_func
+        else:
+            self.eval_function = random_move
+        if game_rules is not None:
+            self.rules = game_rules
+
+    def get_open_moves(board_state):
+        if self.rules is not None:
+            return self.rules.get_valid_moves(board_state)
+        else:
+            return None
+
+    def act(self, state):
+        pass
+
+    def alpha_beta(self):
+        pass
+
+    def minimax(self):
+        pass
+
+    def ab_max(self):
+        pass
+
+    def ab_min(self):
+        pass
+
+
+
+class DQNAgent:
+    def __init__(self, num_states, num_actions, model=None, config=None):
+        if config is not None:
+            pass
+            #TODO Get default params from DeepQ Library
